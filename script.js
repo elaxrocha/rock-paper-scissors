142d28d920823719bacc38476e10925b498290e6
Alexandre Rocha <alexandre.dasir@gmail.com>
2022-07-05 14:44:00 +0200

    Score bug fixed, winner screen configured

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let playerScore = 0
 let robotScore = 0
+let rounds = 0;
+let playedRounds= 0;
 
 
 function computerPlay(){
@@ -11,43 +13,57 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();;;
     if(playerSelection == computerSelection) {
             console.log("It's a draw!")
-            console.log("Your score: " + playerScore);
-            console.log("Robot score: " + robotScore);
+
     } else if(playerSelection == "Rock" && computerSelection == "Scissors"){
             console.log("You Win! Rock beats Scissors.")
-            console.log("Your score: " + playerSocre++);
-            console.log("Robot score: " + robotScore);
+            playerScore++;
+
     }   else if(playerSelection == "Scissors" && computerSelection == "Rock"){
             console.log("You Lose! Rock beats Paper.")
-            console.log("Your score: " + playerScore);
-            console.log("Robot score: " + robotScore++);
+            robotScore++;
+
     }   else if(playerSelection == "Rock" && computerSelection == "Paper"){
             console.log("You Lose! Paper beats Rock.")
-            console.log("Your score: " + playerScore);
-            console.log("Robot score: " + robotScore++);
+            robotScore++;
+
     }   else if(playerSelection == "Paper" && computerSelection == "Rock"){
             console.log("You Win! Paper beats Rock.")
-            console.log("Your score: " + playerScore++);
-            console.log("Robot score: " + robotScore);
+            playerScore++;
+
     }   else if(playerSelection == "Paper" && computerSelection == "Scissors"){
             console.log("You Lose! Scissors beats Paper.")
-            console.log("Your score: " + playerScore);
-            console.log("Robot score: " + robotScore++);
+            robotScore++;
     }   else if(playerSelection == "Scissors" && computerSelection == "Paper"){
             console.log("You Win! Scissors beats Paper.")
-            console.log("Your score: " + playerScore++);
-            console.log("Robot score: " + robotScore);
+            playerScore++;
+    }
+
+    console.log("Your score: " + playerScore);
+    console.log("Robot score: " + robotScore);
+
+    if(playedRounds == rounds){
+        if(playerScore > robotScore){
+            console.log("YOU WON COWBOY!");
+        } else if(robotScore > playerScore){
+            console.log("you lost... how?")
+        } else {
+            console.log("It's a draw chief")
+        }
     }
 }
 
 function game(){
-    for (let i = 0; i < 5; i++) {
+    rounds = parseInt(prompt("Who many rounds you want to play?"));;
 
+    for (let i = 0; i < rounds; i++) {
         playerSelection = prompt("What you choosing cowboy?");
         computerSelection = computerPlay();
+        console.log(" ");
         console.log("You choosed " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " cowboy.");;;;
         console.log("The robot choosed " + computerSelection + ".");
 
+        playedRounds++;
         playRound(playerSelection, computerSelection);
-     }
+        
+     }  
 }
